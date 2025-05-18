2be8526f9530a864f4b40275b0eb48c7e2af9b7f
jaredspillari <jaredspillari@gmail.com>
2025-05-17 18:18:05 -0600

    part 1 and par 2 made

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.Write("What is the magic number?");
+        string random_number = Console.ReadLine();
+        int magic = int.Parse(random_number);
+        Console.Write("What is your guess?");
+        string think = Console.ReadLine();
+        int guess = int.Parse(think);
+
+        if (guess > magic)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guess < magic)
+        {
+            Console.WriteLine($"Higher");
+        }
+        else
+        {
+            Console.WriteLine("You got it");
+        }
+
     }
 }
